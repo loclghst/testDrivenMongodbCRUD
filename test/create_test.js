22d0a7d4dc9e9b691ca3338db27023dfd73e92b8
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-06 19:36:21 +0530

    Modify the test to use asser() and done()

--- test/create_test.js
+++ test/create_test.js
@@ -1,10 +1,15 @@
 const assert = require('assert');
 const User = require('../src/users.js');
 
-describe('Create an user', () =>{
+describe('Create an user', (done) =>{
 	it('Saves a user', () =>{
 		const joe = new User({name : 'Joe'});
-		joe.save();
+		joe.save()
+		   .then(() =>{
+		   	//Has 'joe' been saved successfully to the db
+		   	assert(!joe.isNew);
+		   	done();
+		   });
 		
 	});
 });
