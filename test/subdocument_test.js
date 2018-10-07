4ac92f0ed05f51b8fe79497fdfd535ccba2e077b
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-08 00:38:52 +0530

    Add tests for sub documents

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -15,4 +15,46 @@ describe('Subdocument test', () =>{
 		   		done();
 		   });
 	});
-})
+
+	it('Can add post to an existing User', (done) =>{
+		const joe = new User({
+			name: 'joe',
+			posts: []
+		});
+
+		joe.save()
+		   .then(() => User.findOne({name:'joe'}))
+		   .then((user) =>{
+		   		user.posts.push({title: 'New Post'});
+		   		return user.save();
+		   })
+		   .then(() => User.findOne({name:'joe'}))
+		   .then((user) =>{
+		   		assert(user.posts[0].title === 'New Post');
+		   		done();
+		   });
+	});
+
+	it('Can remove an already existing sub document' ,(done) =>{
+		const joe = new User({
+			name: 'joe',
+			posts : [{title : 'New title'}]
+		});
+
+		joe.save()
+		   .then(() => User.findOne({name: 'joe'}))
+		   .then((user) => {
+		   		const post = user.posts[0];
+		   		// NOTE: When we use .remove on a model instance, we dont need to save it
+		   		// but when we are removing a subdocument from a model instance we need to save it to persist the changes in a db
+		   		post.remove();
+		   		return user.save();
+		   })
+		   .then(() => User.findOne({name: 'joe'}))
+		   .then((user) =>{
+		   		assert(user.posts.length ===0 );
+		   		done();
+		   });
+	});
+
+});
